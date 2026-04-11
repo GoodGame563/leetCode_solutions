309ffc2e1942f6adf4cdf7f00f8a904eef837650
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2026-04-11 15:12:13 +0300

    day 7

--- Python/subarray-sum-equals-k/main.py
+++ Python/subarray-sum-equals-k/main.py
@@ -1,22 +1,14 @@
 from typing import List
-
+from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        lenght = len(nums)
-        if lenght == 1:
-            return 1 if nums[0] == k else 0
-        for i in range(1, lenght): 
-            nums[i] += nums[i-1]
-        left = 0 
-        result = 0
-        for right in range(1, lenght):
-            while left < right and nums[right] - nums[left] > k:
-                left += 1
-            if nums[right] - nums[left] == k:
-                result += 1
+        count, cur_sum = 0, 0  
+        prefix_count = defaultdict(int)
+        prefix_count[0] = 1
+        for num in nums:
+            cur_sum += num
+            count += prefix_count[cur_sum - k]
+            prefix_count[cur_sum] += 1
+        return count
 
-
-        return result + 1
-
-
-print(Solution().subarraySum([1], 0))
+print(Solution().subarraySum([1, 2, 3], 3))
