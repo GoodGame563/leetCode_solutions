b52e1131ef9201ccb52a979b86175616ab77bae6
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2026-04-15 20:02:49 +0300

    104

---
+++ Python/maximum-depth-of-binary-tree/main.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return 1 + max(left, right) 
+
+Solution().maxDepth(TreeNode(3,TreeNode(9),TreeNode(20, TreeNode(15), TreeNode(7))))
+
+print("Hello from maximum-depth-of-binary-tree!")
